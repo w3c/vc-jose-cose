f7669032a739b0c67dc388c0a2e15695ac29b6dd
Gabe <7622243+decentralgabe@users.noreply.github.com>
2024-02-26 11:05:16 -0800

    Fix lints (#245)
    * html fixes
    
    * update cdn
    
    * one more
    
    * remove fors
    
    * point to latest
    
    * update
    
    * update plugin to use h2
    
    * update plugin

--- plugin/src/getHtml.js
+++ plugin/src/getHtml.js
@@ -4,8 +4,8 @@ export const getCombinedHtml = ({ index, coseExample, jwtExample, sdJwtExample }
     return `
 <div class="vc-jose-cose-tabbed">
     <input type="radio" id="vc-jose-cose-tab-${index}-cose" name="vc-jose-cose-tabs-${index}" checked="checked">
-    <input type="radio" id="vc-jose-cose-tab-${index}-jwt" name="vc-jose-cose-tabs-${index}" >
-    <input type="radio" id="vc-jose-cose-tab-${index}-sd-jwt" name="vc-jose-cose-tabs-${index}" >
+    <input type="radio" id="vc-jose-cose-tab-${index}-jwt" name="vc-jose-cose-tabs-${index}">
+    <input type="radio" id="vc-jose-cose-tab-${index}-sd-jwt" name="vc-jose-cose-tabs-${index}">
     <ul class="vc-jose-cose-tabs">
       <li class="vc-jose-cose-tab">
         <label for="vc-jose-cose-tab-${index}-cose">COSE</label>
@@ -34,7 +34,7 @@ export const getJwtHtml = ({ index, jwtExample })=>{
 <div class="vc-jose-cose-jwt-tabbed">
     <ul class="vc-jose-cose-jwt-tabs">
       <li class="vc-jose-cose-jwt-tab">
-        <label for="vc-jose-cose-jwt-tab-${index}-jwt">JWT</label>
+        <label>JWT</label>
       </li>
     </ul>
     <div class="vc-jose-cose-jwt-tab-content">
@@ -48,7 +48,7 @@ export const getSdJwtHtml = ({ index, sdJwtExample })=>{
 <div class="vc-jose-cose-sd-jwt-tabbed">
     <ul class="vc-jose-cose-sd-jwt-tabs">
       <li class="vc-jose-cose-sd-jwt-tab">
-        <label for="vc-jose-cose-sd-jwt-tab-${index}-sd-jwt">SD-JWT</label>
+        <label>SD-JWT</label>
       </li>
     </ul>
     <div class="vc-jose-cose-sd-jwt-tab-content">
@@ -62,7 +62,7 @@ export const getCoseHtml = ({ index, coseExample })=>{
 <div class="vc-jose-cose-cose-tabbed">
     <ul class="vc-jose-cose-cose-tabs">
       <li class="vc-jose-cose-cose-tab">
-        <label for="vc-jose-cose-cose-tab-${index}-cose">COSE</label>
+        <label>COSE</label>
       </li>
     </ul>
     <div class="vc-jose-cose-cose-tab-content">
